05c98f9141861f67fafa80b5cc83c50f41cfecc7
fov42550564 <42550564@qq.com>
2016-11-15 21:54:22 +0800

    完成联系人跳转

--- project/App/modules/contact/index.js
+++ project/App/modules/contact/index.js
@@ -6,11 +6,13 @@ var {
     Text,
     Image,
     StyleSheet,
+    TouchableHighlight,
 } = ReactNative;
 
 var Subscribable = require('Subscribable');
 var CacheImage = require('@remobile/react-native-cache-image');
 var IndexedListView =  require('@remobile/react-native-indexed-listview');
+var MessageInfo = require('../message/MessageInfo.js');
 
 module.exports = React.createClass({
     mixins: [Subscribable.Mixin],
@@ -27,20 +29,28 @@ module.exports = React.createClass({
             list: Object.assign({}, app.userMgr.groupedUsers),
         });
     },
+    showMessageInfo: function(type, targetid) {
+        app.navigator.push({
+            component: MessageInfo,
+            passProps: {type, targetid},
+        });
+    },
     renderRow(obj) {
         var {username, online, head, userid} = app.userMgr.users[obj];
         var url = app.route.ROUTE_USER_HEAD(head);
         return (
-            <View style={styles.row}>
-                <CacheImage
-                    resizeMode='stretch'
-                    defaultImage={app.img.personal_default_head}
-                    url={url}
-                    style={styles.avatar}
-                    cacheId={'userhead_'+userid}
-                    />
-                <Text style={[styles.username, {color: online?'green':'gray'}]}>{username}</Text>
-            </View>
+            <TouchableHighlight underlayColor="#CFCFCF" onPress={this.showMessageInfo.bind(null, app.messageMgr.USER_TYPE, userid)}>
+                <View style={styles.row}>
+                    <CacheImage
+                        resizeMode='stretch'
+                        defaultImage={app.img.personal_default_head}
+                        url={url}
+                        style={styles.avatar}
+                        cacheId={'userhead_'+userid}
+                        />
+                    <Text style={[styles.username, {color: online?'green':'gray'}]}>{username}</Text>
+                </View>
+            </TouchableHighlight>
         )
     },
     render() {
